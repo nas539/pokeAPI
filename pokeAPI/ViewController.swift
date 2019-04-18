d3f0328734523e05fa4a7f2e2c6510bedf4afd38
nicholasSharp539 <naas8905@gmail.com>
2019-04-18 19:45:18 -0400

    Initial commit

--- pokeAPI/ViewController.swift
+++ pokeAPI/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Character: Decodable {
+    
+    let name: String
+    let id: Int
+    
+}
 
+
+class ViewController: UIViewController {
+    @IBOutlet weak var view: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        fetchPokemonJSON { (res) in
+            switch res {
+            case .success(let characters):
+                .characters.forEach ({ (character) in
+                    print(character.name)
+                })
+            case .failure(let err):
+                print("Failure to fetch charcter", err)
+                
+            }
+        }
+        
     }
-
-
+    fileprivate func fetchPokemonJSON(completion: @escaping(Result<[Character], err>) -> ()) {
+        
+        
+        let urlString = "PokemonExample.json"
+        guard let url = URL(string: urlString) else {return}
+        
+        URLSession.shared.dataTask(with: url) {
+            (data, resp, err) in
+            
+            if let err = err {
+                completion(.failure(err))
+                completion(nil, err)
+                return
+            }
+            do {
+                let characters = try JSONDecoder().decode([Character].self, from: data!)
+                completion(.success(characters))
+                completion(characters, nil)
+                
+            } catch let jsonError{
+                completion(.failure(jsonError))
+                completion(nil, jsonError)
+            }
+            
+            
+            }.resume()
+        
+        
+    }
+    
+    @IBOutlet weak var action: UITableView!
 }
+
 
